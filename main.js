1a09552fc1d394fa83b47729cde6134dac22af7f
Alexandre-Georges <alexandre.georges.ra@gmail.com>
2016-09-16 15:34:01 -0700

    Revert "Header moved"
    This reverts commit 4719e12421a7bb96a43c3d3adaea9c1758449dfe.

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ var options = {
 
 https.createServer(options, function (request, response) {
 
+    // Website you wish to allow to connect
+    response.setHeader('Access-Control-Allow-Origin', 'https://ageo-amp.heroku.com');
+
     var filePath = '.' + request.url;
     if (filePath == './')
         filePath = './index.html';
@@ -53,7 +56,6 @@ https.createServer(options, function (request, response) {
             }
         }
         else {
-            response.setHeader('Access-Control-Allow-Origin', 'https://ageo-amp.heroku.com');
             response.writeHead(200, { 'Content-Type': contentType });
             response.end(content, 'utf-8');
         }
